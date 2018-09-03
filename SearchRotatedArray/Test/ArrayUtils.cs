b6021e1804031d1c7e8ab2c8bcb2fd95e8f27dbe
anupriyachaudhary <anupriyachaudhary@gmail.com>
2018-09-03 12:01:08 +0530

    Added another project

---
+++ SearchRotatedArray/Test/ArrayUtils.cs
@@ -0,0 +1,21 @@
+namespace Test.TestUtils
+{
+    public static class ArrayTestUtils
+    {
+        public static bool ArraysEqual(int[] a1, int[] a2)
+        {
+            if (a1.Length == a2.Length)
+            {
+                for (int i = 0; i < a1.Length; i++)
+                {
+                    if (a1[i] != a2[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
